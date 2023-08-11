ffed83b6f76d13942214e1e0dee5a301e919fc17
Sachin Acharya <acharyaraj71+github_project@gmail.com>
2023-08-11 09:33:52 +0545

    - Edititedt

--- statics/js/app.js
+++ statics/js/app.js
@@ -4,28 +4,43 @@ import "../css/progressbar.scss"
 let spikeWrapper = document.querySelector(".spikes")
 let spikes = []
 
-for(let i = 0; i < 360 ; i += 3) {
-    let spike = document.createElement('span')
-    spike.className = 'spike'
-    spike.style.transform = `rotate(${i}deg) translateY(-135px)`
-    spikeWrapper.appendChild(spike)
+const spike_max_angle = 360;
+const spike_step = 3
+const total_spike_count = spike_max_angle / spike_step
 
-    spikes.push(spike)
+for (let i = 0; i < total_spike_count; i++) {
+    const angle_factor = (i / total_spike_count) * spike_max_angle
+    let spike = document.createElement('span')
+        spike.className = 'spike'
+        spike.style.transform = `rotate(${angle_factor}deg) translateY(-135px)`
+        spikeWrapper.appendChild(spike)
+    
+        spikes.push(spike)
 }
 
-if (spikes.length > 0) {
-    let n = spikes.length
-    let i = 0
+function callme(){    
+    if (spikes.length > 0) {
+        let n = spikes.length
+        let i = 0
 
-    const interval = setInterval(() => {
-        spikes[i].className = "spike active"
-        let percent = Math.ceil(((i + 1) / n) * 100)
-        document.querySelector('.value').innerHTML = percent + "%"
-        i++
-        if(i === n) {
-            document.querySelector('.value').innerHTML = "0%"
-            i = 0
-            spikes.forEach(spike => spike.className = "spike")
-        }
-    }, 200)
+        let interval = setInterval(async () => {
+            spikes[i].className = "spike active"
+            let percent = Math.ceil(((i + 1) / n) * 100)
+            document.querySelector('.value').innerHTML = percent + "%"
+            ++i
+            if(i == n) {
+                clearInterval(interval)
+                await sleep(1000)
+                document.querySelector('.value').innerHTML = "0%"
+                i = 0
+                spikes.forEach(spike => spike.className = "spike")
+                callme()
+            }
+        }, 200)
+    }
+}
+callme()
+
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
 }
